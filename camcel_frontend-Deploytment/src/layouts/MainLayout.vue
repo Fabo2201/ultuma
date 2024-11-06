73783549af59f0b89c10b02e4bc7890d0e8d31fa
Fabo2201 <167098660+Fabo2201@users.noreply.github.com>
2024-11-05 22:13:42 -0300

    Add files via upload

---
+++ camcel_frontend-Deploytment/src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="hHh Lpr lff" padding="10">
+    
+    <menu-toolbar-layout>
+        <router-view />
+    </menu-toolbar-layout>
+  </q-layout>
+</template>
+
+<script setup>
+import MenuToolbarLayout from './MenuToolbarLayout.vue';
+import { useUserStore } from "../store/user.store"
+import { useRouter } from 'vue-router';
+import { watchEffect } from 'vue';
+
+const userStore = useUserStore()
+const router = useRouter()
+
+
+
+</script>
+
